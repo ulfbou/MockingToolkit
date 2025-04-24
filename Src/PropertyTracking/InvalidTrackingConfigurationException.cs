e366c0f05e1bbba5d1d3cee3cc7027aefa21fded
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-04-24 17:27:04 +0200

    Fix: Add inner exception support to InvalidTrackingConfigurationException
    This commit adds a constructor overload to the `InvalidTrackingConfigurationException` class that allows specifying an inner exception. This provides more context when an invalid tracking configuration exception is thrown due to another underlying exception.

--- Src/PropertyTracking/InvalidTrackingConfigurationException.cs
+++ Src/PropertyTracking/InvalidTrackingConfigurationException.cs
@@ -4,7 +4,7 @@
 namespace MockingToolkit.PropertyTracking
 {
     /// <summary>
-    /// Represents an exception thrown when an invalid property tracking configuration is detected.
+    /// Exception thrown when the property tracking configuration is invalid.
     /// </summary>
     public class InvalidTrackingConfigurationException : Exception
     {
@@ -13,5 +13,12 @@ namespace MockingToolkit.PropertyTracking
         /// </summary>
         /// <param name="message">The message that describes the error.</param>
         public InvalidTrackingConfigurationException(string message) : base(message) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidTrackingConfigurationException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
+        public InvalidTrackingConfigurationException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
