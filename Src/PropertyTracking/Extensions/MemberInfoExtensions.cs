6f48028d7d29071146fc50a73b3705fb180e75cf
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-04-24 09:47:18 +0200

    refactor: Consolidate IsPublic extension methods
    This commit refactors the `IsPublic` extension methods for `MemberInfo` and `PropertyInfo`.
    
    - Removes the `IsPublic` extension method from `MemberInfoExtensions`. The logic for determining public accessibility was incorrect as it only checked the `DeclaringType`'s visibility.
    - Retains the `IsPublic` extension method in `PropertyInfoExtensions` as it correctly checks the visibility of both the getter and setter methods.
    
    This change ensures a more accurate and consistent way of determining the public accessibility of properties within the property tracking feature.

---
+++ Src/PropertyTracking/Extensions/MemberInfoExtensions.cs
@@ -0,0 +1,32 @@
+﻿//// Copyright (c) Ulf Bourelius. All rights reserved.
+//// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockingToolkit.PropertyTracking.Extensions
+{
+    public static class MemberInfoExtensions
+    {
+        // IsPublic
+        public static bool IsPublic(this MemberInfo memberInfo)
+        {
+            if (memberInfo == null)
+            {
+                throw new ArgumentNullException(nameof(memberInfo));
+            }
+
+            if (memberInfo.DeclaringType == null)
+            {
+                throw new InvalidOperationException("DeclaringType is null.");
+            }
+
+            return memberInfo.DeclaringType.IsPublic;
+        }
+    }
+}
---
+++ Src/PropertyTracking/Extensions/PropertyInfoExtensions.cs
@@ -0,0 +1,21 @@
+﻿//// Copyright (c) Ulf Bourelius. All rights reserved.
+//// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Reflection;
+
+namespace MockingToolkit.PropertyTracking.Extensions
+{
+    public static class PropertyInfoExtensions
+    {
+        // IsPublic
+        public static bool IsPublic(this PropertyInfo propertyInfo)
+        {
+            if (propertyInfo == null)
+            {
+                throw new ArgumentNullException(nameof(propertyInfo));
+            }
+
+            return propertyInfo.GetMethod?.IsPublic == true || propertyInfo.SetMethod?.IsPublic == true;
+        }
+    }
+}
